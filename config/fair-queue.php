406aeb75bc5ac1980f215c25d141ef12575c57bb
Hamed Kamrava <hamed@aloware.com>
2022-03-28 23:08:42 +0300

    add descriptions to config file

--- config/fair-queue.php
+++ config/fair-queue.php
@@ -13,7 +13,23 @@ return [
     */
     'middleware' => ['web'],
 
+    /*
+    |--------------------------------------------------------------------------
+    | FairQueue Redis DB
+    |--------------------------------------------------------------------------
+    |
+    | If you have a seperated redis connection for fair-queue, you can specify
+    | Redis connection name here
+    */
     'database'   => env('FAIR_QUEUE_REDIS_DB', 'default'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | FairQueue Redis DB
+    |--------------------------------------------------------------------------
+    |
+    | If you want to have a custom redis key prefix for your fair-queue queues
+    | you can set it here
+    */
     'key_prefix' => env('FAIR_QUEUE_KEY_PREFIX', 'fair-queue'),
 ];
